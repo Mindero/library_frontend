f088fe5026a7f22a31671b39bb6361a2f9e5d053
vadim <vadimyanchenko64@gmail.com>
2024-12-17 13:11:01 +0400

    feat: добит profilePage

--- src/components/ProfilePage/ProfilePage.tsx
+++ src/components/ProfilePage/ProfilePage.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import { getProfileInfo } from './ProfileService';
 import { useSelector } from 'react-redux';
 import { userJwtSelector } from '../../reducer/userStore/reducer';
+import ProfileForm from './ProfileForm';
 
 export const ProfilePage = () => {
-  getProfileInfo(useSelector(userJwtSelector)).then(profile => {
-    console.log(profile);
-    return (
-      <div>
-        Profile info
-      </div>
-    )
-  });
+
+  const [profile, setProfile] = useState<ProfileForm | null>(null);
+  const jwt: string | null= useSelector(userJwtSelector);
+
+  useEffect(() => {
+    getProfileInfo(jwt).then(profile => {
+      setProfile(profile);
+    })
+  }, []);
+
+  return (
+    <div>
+      Profile info
+      <p>Name: {profile?.name} </p>
+      <p>Email: {profile?.email} </p>
+      <p>Phone number: {profile?.phone_number} </p>
+      <p>Created date: {profile?.created_date.toLocaleString()} </p>
+    </div>
+  )
 }
